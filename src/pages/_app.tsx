0ce041c0c5ddf97747ff2772fce14f935f6760ef
鈴木慎乃介 <suzukimakotonosuke@suzukimukenoMBP.lan>
2023-03-30 19:35:23 +0900

    chakraUI インストールする

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import type { AppProps } from 'next/app'
 import { initializeFirebaseApp } from '../lib/firebase/firebase'
+import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 
 initializeFirebaseApp()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
