2c4c8b828a9c9109016b7e7b92915e51086b4db6
鈴木慎乃介 <suzukimakotonosuke@suzukimakotonosukenoMacBook-Pro.local>
2023-03-31 09:24:30 +0900

    ChakraProviderコメントアウト

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
 import type { AppProps } from 'next/app'
 import { initializeFirebaseApp } from '../lib/firebase/firebase'
-import { ChakraProvider } from '@chakra-ui/react'
+// import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 
 initializeFirebaseApp()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    // <ChakraProvider>
       <Component {...pageProps} />
-    </ChakraProvider>
+    // </ChakraProvider>
   )
 }
