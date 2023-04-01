9e793d30d78aa0e422e799b73c8e5461b4991421
鈴木慎乃介 <suzukimakotonosuke@suzukimukenoMBP.lan>
2023-04-02 01:05:52 +0900

    編集ページ作っておく

---
+++ src/pages/todos/edit/[id].tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Edit() {
+    return (
+        <div>
+            <Link href="/todos">戻る</Link>
+        </div>
+    )
+}
