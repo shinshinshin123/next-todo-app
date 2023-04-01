9138952eae9fc112598d419074d0eff6444e2299
鈴木慎乃介 <suzukimakotonosuke@suzukimukenoMBP.lan>
2023-04-01 09:26:41 +0900

    chakra-uiが治った

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from 'next/app'
-// import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
+// import * as FramerMotion from 'framer-motion'
 import React from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+  // const {  motion, useI}
+
   return (
-    // <ChakraProvider>
+    <ChakraProvider>
       <Component {...pageProps} />
-    // </ChakraProvider>
+    </ChakraProvider>
   )
 }
