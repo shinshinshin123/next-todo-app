2de73910a769fc20efb6fd4fa430bd21d0116009
鈴木慎乃介 <suzukimakotonosuke@suzukimukenoMBP.lan>
2023-04-03 15:04:45 +0900

    必要なものをimport

--- src/pages/todos.tsx
+++ src/pages/todos.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Link from "next/link";
+import { useState, useEffect } from "react";
+import { collection, getDocs } from "@firebase/firestore";
+import { db } from "src/lib/firebase";
 
 export default function Todos() {
-    return (
-        <>
-            <h1>TODO一覧ページ</h1>
-            <Link href="/todos/create">追加</Link>
-        </>
-    )
+  return (
+    <>
+      <h1>TODO一覧ページ</h1>
+      <Link href="/todos/create">TODO作成</Link>
+    </>
+  )
 }
