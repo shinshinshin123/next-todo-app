d156bafcc5348ce1e9bc3059ba51427f13a0f22e
鈴木慎乃介 <suzukimakotonosuke@suzukimukenoMBP.lan>
2023-04-07 19:24:05 +0900

    ui部分作成

--- src/pages/todos/edit/[id].tsx
+++ src/pages/todos/edit/[id].tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import { useFirestore } from "reactfire";
 
 export default function Edit() {
   return (
@@ -10,16 +9,34 @@ export default function Edit() {
       <h1>Edit Todo</h1>
       <Link href="/todos">戻る</Link>
       <form onSubmit={editSubmit}>
-       <div>
-         <label htmlFor="title">タイトル</label>
-         <input
-           type="text"
-           id="title"
-           name="title"
-           value={formValues.title}
-           onChange={handleChange}
-         />
-       </div>
+        <div>
+          <label htmlFor="title">タイトル</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={todoTitle}
+            onChange={handleTitleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="content">内容</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={todoContent}
+            onChange={handleContentChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="status">ステータス</label>
+          <select id="status" value={todoStatus} onChange={handleStatusChange}>
+            <option value="未完了">未完了</option>
+            <option value="途中">途中</option>
+            <option value="完了">完了</option>
+          </select>
+        </div>
       </form>
       <button type="submit">保存</button>
     </div>
