00f702eefac048a263359db0a6c15b256ab41ed7
鈴木慎乃介 <suzukimakotonosuke@suzukimukenoMBP.lan>
2023-04-07 10:14:40 +0900

    ui部分

--- src/pages/todos/[id].tsx
+++ src/pages/todos/[id].tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { doc, getFirestore } from "firebase/firestore";
 
 export default function Show() {
+
   return (
     <div>
+      <h3>タイトル</h3>
+      <p>{todo.title}</p>
+      <h3>内容</h3>
+      <p>{todo.content}</p>
+      <h3>ステータス</h3>
+      <p>{todo.status}</p>
+      <h3>作成日時</h3>
+      <p>{todo.createdAt.toString().slice(0, -16)}</p>
       <Link href="/todos">戻る</Link>
     </div>
   )
