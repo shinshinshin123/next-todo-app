dfc5b3dd07acd8a42b015199e7eeac06b49ffa64
鈴木慎乃介 <suzukimakotonosuke@suzukimukenoMBP.lan>
2023-04-08 08:53:22 +0900

    todo見つかるまでのloadingを差し込む

--- src/pages/todos/edit/[id].tsx
+++ src/pages/todos/edit/[id].tsx
@@ -1,9 +1,25 @@
 import React from "react";
 import Link from "next/link";
 import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/router"
+import { collection, addDoc, serverTimestamp, getFirestore } from "@firebase/firestore";
+
 
 export default function Edit() {
+  const router = useRouter();
+  const { id } = router.query;
+  const firebase = getFirestore();
+  const [todo, setTodo] = useState({
+    title: "",
+    content: "",
+    status: ""
+  })
+
+  // 該当するtodoの情報を取得するまでの間にloading出す。
+  if (!todo) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div>
       <h1>Edit Todo</h1>
@@ -15,23 +31,21 @@ export default function Edit() {
             type="text"
             id="title"
             name="title"
-            value={todoTitle}
+            value={todo.title}
             onChange={handleTitleChange}
           />
         </div>
         <div>
           <label htmlFor="content">内容</label>
           <input
-            type="text"
-            id="title"
-            name="title"
-            value={todoContent}
+            id="content"
+            value={todo.content}
             onChange={handleContentChange}
           />
         </div>
         <div>
           <label htmlFor="status">ステータス</label>
-          <select id="status" value={todoStatus} onChange={handleStatusChange}>
+          <select id="status" value={todo.status} onChange={handleStatusChange}>
             <option value="未完了">未完了</option>
             <option value="途中">途中</option>
             <option value="完了">完了</option>
