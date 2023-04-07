5f30092ae7d6f1e867f2623e0c18cb8284bab4ce
鈴木慎乃介 <suzukimakotonosuke@suzukimakotonosukenoMacBook-Pro.local>
2023-04-07 14:47:22 +0900

    ui途中

--- src/pages/todos/edit/[id].tsx
+++ src/pages/todos/edit/[id].tsx
@@ -1,11 +1,27 @@
 import React from "react";
 import Link from "next/link";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { useFirestore } from "reactfire";
 
 export default function Edit() {
-    return (
-        <div>
-            <h1>編集ページ</h1>
-            <Link href="/todos">戻る</Link>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Edit Todo</h1>
+      <Link href="/todos">戻る</Link>
+      <form onSubmit={editSubmit}>
+       <div>
+         <label htmlFor="title">タイトル</label>
+         <input
+           type="text"
+           id="title"
+           name="title"
+           value={formValues.title}
+           onChange={handleChange}
+         />
+       </div>
+      </form>
+      <button type="submit">保存</button>
+    </div>
+  )
 }
