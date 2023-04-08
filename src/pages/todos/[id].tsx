fba0437f6e2b84f2bb2fbd606528d81f3fb58b4c
鈴木慎乃介 <suzukimakotonosuke@suzukimukenoMBP.lan>
2023-04-08 15:12:31 +0900

    ひとまず表示することができた

--- src/pages/todos/[id].tsx
+++ src/pages/todos/[id].tsx
@@ -2,29 +2,26 @@ import React from "react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { getFirestore, collection, doc, getDoc, CollectionReference, DocumentData } from "firebase/firestore";
+import { doc, DocumentData, getDoc } from "firebase/firestore";
+import { db } from "src/lib/firebase";
 
 export default function Show() {
   const router = useRouter();
   const { id } = router.query;
-  const [todo, setTodo] = useState();
+  const [todo, setTodo] = useState<DocumentData | null>(null);
 
-  // useEffect(() => {
-  //   const fetchTodo = async () => {
-  //     const db = getFirestore();
-  //     //コレクションの参照を取得
-  //     const todosRef = collection(db, "todods");
-  //     //Todoのドキュメントの参照を取得
-  //     const todoDoc = doc(todosRef, id);
-  //     //ドキュメントのデータを取得
-  //     const todoData = await getDoc(todoDoc);
-  //     setTodo(todoData.data());
-  //   };
-
-  //   if (id) {
-  //     fetchTodo();
-  //   }
-  // }, [id])
+  useEffect(() => {
+    const fetchTodo = async () => {
+      if(id) {
+        const todoRef = doc(db, "todos", id.toString());
+        const docSnapshot = await getDoc(todoRef);
+        if (docSnapshot.exists()) {
+          setTodo(docSnapshot.data());
+        }
+      }
+    };
+    fetchTodo();
+  }, [id])
 
   if (!todo) {
     return <div>Loading...</div>
@@ -32,14 +29,14 @@ export default function Show() {
 
   return (
     <div>
-      {/* <h3>タイトル</h3>
+      <h3>タイトル</h3>
       <p>{todo.title}</p>
       <h3>内容</h3>
       <p>{todo.content}</p>
       <h3>ステータス</h3>
       <p>{todo.status}</p>
       <h3>作成日時</h3>
-      <p>{todo.createdAt.toString().slice(0, -16)}</p> */}
+      <p>{todo.createdAt.toString().slice(0, -16)}</p>
       <Link href="/todos/edit/${todo.id}">編集する</Link>
       <button type="submit">削除する</button>
       <Link href="/todos">戻る</Link>
