ff36a8df429a3a9db117a9a8dafc410884be8866
鈴木慎乃介 <suzukimakotonosuke@suzukimukenoMBP.lan>
2023-04-08 23:43:01 +0900

    コメント投稿機能のui

--- src/pages/todos/[id].tsx
+++ src/pages/todos/[id].tsx
@@ -39,17 +39,47 @@ export default function Show() {
 
   return (
     <div>
-      <h3>タイトル</h3>
-      <p>{todo.title}</p>
-      <h3>内容</h3>
-      <p>{todo.content}</p>
-      <h3>ステータス</h3>
-      <p>{todo.status}</p>
-      <h3>作成日時</h3>
-      <p>{todo.createdAt.toDate().toLocaleString()}</p>
       <Link href="/todos/edit/${todo.id}">編集する</Link>
       <button onClick={deleteTodo}>削除する</button>
       <Link href="/todos">戻る</Link>
+      <h1>タイトル</h1>
+      <p>{todo.title}</p>
+      <h2>内容</h2>
+      <p>{todo.content}</p>
+      <h2>ステータス</h2>
+      <p>{todo.status}</p>
+      <h2>作成日時</h2>
+      <p>{todo.createdAt.toDate().toLocaleString()}</p>
+      <h2>コメント</h2>
+      <div>
+        <ul>
+          {todo.comments && todo.comments.map((comment :any) => (
+            <li key={comment.id}>
+              <p>{comment.content}</p>
+              <p>{comment.name}</p>
+              <p>{comment.createdAt.toDate().toLocaleString()}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <form onSubmit={handleCommentSubmit}>
+      <div>
+          <label htmlFor="name">名前</label>
+          <input
+            value={commentName}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="名前"
+          />
+        </div>
+        <div>
+          <label htmlFor="content">コメント</label>
+          <textarea
+            value={commentContent}
+            onChange={(e) => setComment(e.target.value)}
+          />
+        </div>
+        <button type="submit">コメントを投稿する</button>
+      </form>
     </div>
   )
 }
