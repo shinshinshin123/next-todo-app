9252d807ed029c070a9c2aeba9166f0ce6bf1924
鈴木慎乃介 <suzukimakotonosuke@suzukimukenoMBP.lan>
2023-04-10 14:30:40 +0900

    ページ作る

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,9 @@ import Link from "next/link"
 
 export default function Todos() {
   return (
-    <>
+    <div>
       <h1>todoアプリ!!!</h1>
       <Link href="/todos">Todo一覧へ</Link>
-    </>
+    </div>
   )
 }
