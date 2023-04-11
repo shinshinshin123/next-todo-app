cc38323b5f90d762b436ea964ebfcc97690eb96e
鈴木慎乃介 <suzukimakotonosuke@suzukimukenoMBP.lan>
2023-04-11 14:40:06 +0900

    ログイン機能のUI部分を作成

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -4,7 +4,36 @@ import Link from "next/link";
 export default function Signin () {
   return (
     <div>
-      <></>
+      <h1>ログイン</h1>
+      <Link href="/todos">戻る</Link>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <label htmlFor="email">Eメール</label>
+          <input
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+            placeholder="Eメール"
+          />
+        </div>
+        <div>
+          <label htmlFor="password">パスワード</label>
+          <input
+            id="email"
+            value={passsword}
+            onChange={handlePasswordChange}
+            placeholder="パスワード"
+          />
+        </div>
+        <div>
+        </div>
+        <button type="submit" disabled={isLaoding}>
+          ログイン
+        </button>
+        <button type="button" onClick={() => signIn('google')}>
+          Googleアカウントでログイン
+        </button>
+      </form>
     </div>
   )
 }
