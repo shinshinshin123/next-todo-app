b669c761a070dd2f2522645c4f00f808521444cf
鈴木慎乃介 <suzukimakotonosuke@suzukimukenoMBP.lan>
2023-04-12 16:08:16 +0900

    新規登録ページを作り始める

---
+++ src/pages/signup.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+export default function Signup () {
+  return (
+    <div>
+      <h1>新規登録</h1>
+    </div>
+  )
+}
